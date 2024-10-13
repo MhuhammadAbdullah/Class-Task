ad2e77d30f6fb537a8b3c594ca7cfbab0541b2d8
Muhammad Abdullah <abaloch0033@gmail.com>
2024-10-13 11:32:13 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
+// QUESTION: 1
+
+// function showDateTime() {
+//     var now = new Date();
+//     var dateTimeString = now.toLocaleString();
+//     document.write("Current Date and Time: " + dateTimeString);
+// }
+
+// showDateTime();
+
+
+// QUESTION: 2
+
+// function greeting() {
+//     var firstName = prompt("Enter your first name")
+//     var laststName = prompt("Enter your last name")
+//     document.write("Welcome! : " + firstName + " "
+//         + laststName)
+// }
+
+// greeting()
+
+
+// QUESTION: 3
+
+// function addTwoNumbers() {
+//     var num1 = parseFloat(prompt("Enter a number:"));
+//     var num2 = parseFloat(prompt("Enter a number:"));
+
+//     var sum = num1 + num2;
+
+//     document.write(num1 + " + " + num2 + " = " + sum);
+// }
+
+// addTwoNumbers();
+
+
 
